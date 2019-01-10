807e494985c2b679e47f9754b1cb9c8e4ead030e
Sander Cokart <112039@student.drenthecollege.nl>
2019-01-10 09:29:47 +0100

    function to remove an entry

---
+++ includes/remove.inc.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'db.inc.php';
+
+$id = $_POST['id'];
+
+if (isset($id)) {
+    $sql = "DELETE FROM `songs` WHERE ID = '$id';";
+    mysqli_query($conn, $sql);
+    }
+
+    header("Location: ../index.php?submit=succes");
+    $conn->close();
