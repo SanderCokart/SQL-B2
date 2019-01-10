9b86789b0dc87b29b3971a724ebf2fab18b89927
Sander Cokart <112039@student.drenthecollege.nl>
2019-01-10 09:29:12 +0100

    created an edit functionality

---
+++ edit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+    <head>
+        <link rel="stylesheet" href="css/master.css">
+        <meta charset="utf-8">
+        <title>EDIT</title>
+    </head>
+    <body>
+        <center><form action="includes/edit.inc.php" method="POST">
+            <input type="text" name="id"placeholder="ID">
+            <br>
+            <input type="text" name="songName"placeholder="Song Name">
+            <br>
+            <input type="text" name="artistName"placeholder="Artist Name">
+            <br>
+            <button type="submit" name="submit">EDIT</button>
+        </form></center>
+    </body>
+</html>
