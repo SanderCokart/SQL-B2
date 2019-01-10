9727132d70eeffecf16cc39b8c4465ad94ef8577
Sander Cokart <112039@student.drenthecollege.nl>
2019-01-10 09:29:39 +0100

    added edit functionality to the background

---
+++ includes/edit.inc.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'db.inc.php';
+
+$id = $_POST['id'];
+$artistName = $_POST['artistName'];
+$songName = $_POST['songName'];
+
+$sql = "UPDATE songs
+SET Artist = '$artistName', Title = '$songName'
+WHERE ID = '$id';";
+
+if (isset($id,$artistName,$songName)) {
+    mysqli_query($conn, $sql);
+}
+header("Location: ../index.php?submit=succes");
